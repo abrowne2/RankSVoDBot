4d8b485a1c44917682ee7cbc3b231f8181686db4
Adam Browne <adamo.browne@gmail.com>
2017-01-04 00:20:59 -0500

    Update aggregation and matchDB

--- aggregation.py
+++ aggregation.py
@@ -1,3 +1,38 @@
 '''
 RankS Match Fetching Here.
 '''
+#need cfscrape to circumvent esea's cloudflare platform.
+import cfscrape
+
+
+def getCurPlayers():
+	standings = 'https://play.esea.net/index.php?s=stats&d=pro'
+	results = cfscrape.create_scraper()
+	data = results.get(standings).content.decode('utf-8')
+	curPos = data.find('/users/'); players = []
+	while curPos != -1:
+		end = data.find('"', curPos)
+		points = data.find('align="right"', curPos)+14; points = data[points:data.find("<",points)]		
+		if points != '-' and points != 'Points':
+			players.append(data[curPos:end]+'?tab=stats')
+		curPos = data.find('/users/', curPos+1)
+	return players[1:]
+
+
+def getCurMatches():
+	players = getCurPlayers()
+	matcher = cfscrape.create_scraper(); matches = []
+	#append /index.php?s=stats&d=match& in front of matches to get their link structure.
+	for player in players:
+		stats = matcher.get('https://play.esea.net'+player).content.decode('utf-8')
+		curPos = stats.find('id=')
+		while curPos != -1:		
+			matchPos = stats.find('"', curPos)+2
+			if stats[matchPos:matchPos+6] == 'Rank S':
+				matches.append(stats[stats.find("id=", curPos)+3:matchPos-2])
+			curPos = stats.find('id=', curPos+1)
+	#get unique matches
+	matches = list(set(matches))
+	print(matches)
+
+getCurMatches()
