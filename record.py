e4214c3f033f216cc5c737c41820691896224109
Adam Browne <adamo.browne@gmail.com>
2017-01-09 01:30:03 -0500

    attempt with R

--- record.py
+++ record.py
@@ -9,12 +9,12 @@ import win32con, time, os
 #0x0C for the '/' which you'll use to record.
 #just catch the info here, to stop the next keystroke from firing...
 more_info = vod.download('8754553')
-win.keybd_event(0x0C, 0,0)
-time.sleep(.5)
-win.keybd_event(0x0C,0,2)
+win.keybd_event(0x13, 0,0,0)
+time.sleep(2)
+win.keybd_event(0x13,0,win32con.KEYEVENTF_KEYUP,0)
 time.sleep(more_info[1])
-win.keybd_event(0x0C, 0,0)
+win.keybd_event(0x13, 0,0,0)
 time.sleep(.5)
-win.keybd_event(0x0C,0,2)
+win.keybd_event(0x13,0,win32con.KEYEVENTF_KEYUP,0)
 time.sleep(5)
 os.system("taskkill /f /im csgo.exe")
