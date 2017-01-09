b9cb25f3c53f0b39d5f5c6fdea06b4753e214e27
Adam Browne <adamo.browne@gmail.com>
2017-01-08 23:09:30 -0500

    Test recording algorithm

--- download.py
+++ download.py
@@ -19,15 +19,15 @@ def download(match_id):
 		#extracts the info about the match and places it in more_info
 		more_info = [info.strip() for info in data[next+1:data.find('<acronym', next)-1].split('/')]
 		#refine the information down into MM/DD/YY TIME, Duration, Mapchoice, demo filename
-		more_info = [more_info[0]+'/'+more_info[1]+'/'+more_info[2], more_info[3], more_info[4], 'esea_match_'+match_id]
+		more_info = [more_info[0]+'/'+more_info[1]+'/'+more_info[2], sec_duration(more_info[3].split(':')), more_info[4], 'esea_match_'+match_id]
 		#downloads the match archive, and then once completed, extracts it.
 		dl = cfscrape.create_scraper()
-		archive_data = dl.get(dl_link).content
-		
+		archive_data = dl.get(dl_link).content		
 		with open('match.zip', 'wb') as f:
 			f.write(archive_data)
 		Archive('match.zip').extractall('C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo')
 		b = modifyConfig(match_id)
+		time.sleep(45)
 		return more_info
 	return []
 
@@ -44,3 +44,12 @@ def modifyConfig(match_id):
 	path = r'C:\"Program Files (x86)"\Steam\Steam.exe -applaunch 730'
 	os.system("start " + path)
 	return 'done'
+
+def sec_duration(hhmmss):
+	total = 1
+	if len(hhmmss) == 2:
+		total = 60 * int(hhmmss[0])
+		total += int(hhmmss[1])
+	else:
+		total = (3600 * int(hhmmss[0])) + (60 * int(hhmmss[1])) + int(hmmss[2])
+	return total
--- record.py
+++ record.py
@@ -3,6 +3,18 @@ Write some code here to begin the recording process.
 '''
 
 import download as vod
+import win32api as win
+import win32con, time, os
 
-
-vod.download('8754553')
+#0xBF for the '/' which you'll use to record.
+#just catch the info here, to stop the next keystroke from firing...
+more_info = vod.download('8754553')
+win.keybd_event(0xBF, 0,0,0)
+time.sleep(.05)
+win.keybd_event(0xBF,0,win32con.KEYEVENTF_KEYUP,0)
+time.sleep(more_info[1])
+win.keybd_event(0xBF, 0,0,0)
+time.sleep(.05)
+win.keybd_event(0xBF,0,win32con.KEYEVENTF_KEYUP,0)
+time.sleep(5)
+os.system("taskkill /f /im csgo.exe")
