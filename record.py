8d3c066c1e54dcb4a2dc8b8dfe23a3bd4c2b8ce2
Adam Browne <adamo.browne@gmail.com>
2017-01-09 00:01:16 -0500

    Test xinfinity

--- record.py
+++ record.py
@@ -3,15 +3,16 @@ Write some code here to begin the recording process.
 '''
 
 import download as vod
-import time, os, win32com.client
+import time, os, win32api, win32ui
 
 #0xBD for the '-' which you'll use to record.
 #just catch the info here, to stop the next keystroke from firing...
-more_info = vod.download('8754553')
-shell = win32com.client.Dispatch("WScript.Shell")
-shell.AppActivate("Counter-Strike: Global Offensive")
-shell.SendKeys("-")
-time.sleep(more_info[1])
-shell.SendKeys("-")
-time.sleep(5)
-os.system("taskkill /f /im csgo.exe")
+# more_info = vod.download('8754553')
+shell = win32ui.FindWindow(None, "Counter-Strike: Global Offensive")
+shell.SetForegroundWindow()
+shell.SetFocus()
+win32api.keybd_event(0x6D,0,0)
+# time.sleep(more_info[1])
+# win32api.keybd_event(0x6D,0,0)
+# time.sleep(5)
+# os.system("taskkill /f /im csgo.exe")
