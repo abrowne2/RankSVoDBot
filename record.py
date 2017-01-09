fb369037356e24ea175afe8868d05be7725d6ca8
Adam Browne <adamo.browne@gmail.com>
2017-01-09 00:27:13 -0500

    try something here.

--- record.py
+++ record.py
@@ -3,15 +3,17 @@ Write some code here to begin the recording process.
 '''
 
 import download as vod
-import time, os, win32api, win32ui
+from scite import find_window, send_input, make_input_objects
 
+import time, os
 #0xBD for the '-' which you'll use to record.
 #just catch the info here, to stop the next keystroke from firing...
 # more_info = vod.download('8754553')
-shell = win32ui.FindWindow(None, "Counter-Strike: Global Offensive")
-shell.SetForegroundWindow()
-shell.SetFocus()
-win32api.keybd_event(0x6D,0,0)
+csgo = find_window("Counter-Strike: Global Offensive")
+l_keys = [ ]; inp = ((0x6D,0))
+l_keys.extend(inp)
+csinputs = make_input_objects(l_keys)
+t = send_input(csgo, csinputs)
 # time.sleep(more_info[1])
 # win32api.keybd_event(0x6D,0,0)
 # time.sleep(5)
