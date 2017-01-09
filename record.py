c6f1d89b53d48b769138081040d4a50ec5d8f7b3
Adam Browne <adamo.browne@gmail.com>
2017-01-08 23:43:59 -0500

    new method of sending keys.

--- record.py
+++ record.py
@@ -3,18 +3,15 @@ Write some code here to begin the recording process.
 '''
 
 import download as vod
-import win32api as win
-import win32con, time, os
+import time, os, win32com.client
 
 #0xBD for the '-' which you'll use to record.
 #just catch the info here, to stop the next keystroke from firing...
 more_info = vod.download('8754553')
-win.keybd_event(0xBD, 0,0,0)
-time.sleep(.05)
-win.keybd_event(0xBD,0,win32con.KEYEVENTF_KEYUP,0)
+shell = win32com.client.Dispatch("WScript.Shell")
+shell.AppActivate("Counter-Strike: Global Offensive")
+shell.SendKeys("-")
 time.sleep(more_info[1])
-win.keybd_event(0xBD, 0,0,0)
-time.sleep(.05)
-win.keybd_event(0xBD,0,win32con.KEYEVENTF_KEYUP,0)
+shell.SendKeys("-")
 time.sleep(5)
 os.system("taskkill /f /im csgo.exe")
