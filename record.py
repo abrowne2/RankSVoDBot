f09cb95d3c73cf8b95a4e6ebd4c576c218486afe
Adam Browne <adamo.browne@gmail.com>
2017-01-09 01:45:06 -0500

    update further..

--- record.py
+++ record.py
@@ -4,17 +4,16 @@ Write some code here to begin the recording process.
 
 import download as vod
 import win32api as win
-import win32con, time, os
+import win32con, time, os, win32com.client
+from input import PressKey, ReleaseKey
 
 #0x0C for the '/' which you'll use to record.
 #just catch the info here, to stop the next keystroke from firing...
-more_info = vod.download('8754553')
-win.keybd_event(0x13, 0,0,0)
-time.sleep(2)
-win.keybd_event(0x13,0,win32con.KEYEVENTF_KEYUP,0)
-time.sleep(more_info[1])
-win.keybd_event(0x13, 0,0,0)
-time.sleep(.5)
-win.keybd_event(0x13,0,win32con.KEYEVENTF_KEYUP,0)
-time.sleep(5)
-os.system("taskkill /f /im csgo.exe")
+# more_info = vod.download('8754553')
+shell = win32com.client.Dispatch("WScript.Shell")
+shell.AppActivate("Counter-Strike: Global Offensive")
+sleep(2)
+PressKey(0x13)
+time.sleep(.2)
+ReleaseKey(0x13)
+# os.system("taskkill /f /im csgo.exe")
