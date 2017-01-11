b9ab93b727f45a3778da6aefe11b485ef648805f
Adam Browne <adamo.browne@gmail.com>
2017-01-10 22:34:55 -0500

    test again

--- upload.py
+++ upload.py
@@ -2,7 +2,9 @@ import os, glob, subprocess
 
 def upload(vid_info):
 	video = glob.glob(r'C:\\Users\\Adam\\Videos\\*.avi')[0]
+	print(video)
 	file = 'C:\\Users\\Adam\\Videos\\' + video
+	print(file)
 	title = vid_info[0]; desc = vid_info[1]; keywords = vid_info[2]
 	cmd = 'python ' + os.getcwd() + '\ytapi.py --file="'+file+'" --title="'+title+'" --description="'+desc+'" --keywords="'+keywords+'" --category="20"'
 	u = subprocess.Popen(cmd, shell=True)
