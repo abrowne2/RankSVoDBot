85840c9a5b6dae2d402683e051b037daa62245ef
Adam Browne <adamo.browne@gmail.com>
2017-01-10 23:01:02 -0500

    test auth

--- upload.py
+++ upload.py
@@ -3,7 +3,7 @@ import os, glob, subprocess
 def upload(vid_info):
 	file = glob.glob(r'C:\Users\Adam\Videos\*.avi')[0]
 	title = vid_info[0]; desc = vid_info[1]; keywords = vid_info[2]
-	cmd = 'python ' + os.getcwd() + '\ytapi.py --file="'+file+'" --title="'+title+'" --description="'+desc+'" --keywords="'+keywords+'" --category="20"'
+	cmd = 'python ' + os.getcwd() + '\ytapi.py --file="'+file+'" --title="'+title+'" --description="'+desc+'" --keywords="'+keywords+'" --category="20" --noauth_local_webserver'
 	u = subprocess.Popen(cmd, shell=True)
 	b = u.communicate()
 	isUploading = u.wait()
